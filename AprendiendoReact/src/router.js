887458eab34a403d7f7e1178c1f4e141aec75b4c
Joshua A. Díaz Robayna <jadrdevcm@gmail.com>
2020-06-22 15:17:44 +0100

    Update router.js
    Cambios

--- AprendiendoReact/src/router.js
+++ AprendiendoReact/src/router.js
@@ -1,92 +1,23 @@
-import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import SeccionPruebas from "./components/SeccionPruebas";
+import MiComponente from "./components/MiComponente";
+import Peliculas from "./components/Peliculas";
 
-/* Importar componentes */
-import SeccionPruebas from './components/SeccionPruebas'
-import MiComponente from './components/MiComponente';
-import Peliculas from './components/Peliculas';
-import error from "./components/error";
-import Header from "./components/Header";
-import Slider from "./components/Slider";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
-
-
-class Router extends Component{
-    render() {
-        var buttonString = "Ver más";
-        return(
-            <BrowserRouter>
-
-                <Header />
-
-                <Slider
-                    title="Bienvenido al curso de React"
-                    btn= {buttonString}
-
-                />
-
-                <div className="center">
-
-
-                    {/* Configurar rutas */}
-                    <Switch>
-                        <Route exact path="/" component={Peliculas} />
-                        <Route exact path="/home" component={Peliculas} />
-                        <Route exact path="/prueba-ruta" component={SeccionPruebas} />
-                        <Route exact path="/segunda-ruta" component={MiComponente} />
-
-                        <Route exact path="/pagina-1" render={() => (
-                            <React.Fragment>
-                            <h1> Hola Mundo </h1>
-                            <MiComponente saludo="Hola Amigo" />
-
-                            </React.Fragment>
-                        )} />
-
-                        <Route exact path="/pruebas/:nombre/:apellidos?" render={(props) => {
-                            var nombre = props.match.params.nombre;
-                            var apellidos = props.match.params.apellidos;
-
-                            return (
-                                    <div id="content">
-                                        <h1 className="subheader">Página de pruebas</h1>
-                                        <h2>
-                                            {nombre && !apellidos &&
-                                            <React.Fragment>
-                                                {nombre}
-                                            </React.Fragment>
-                                            }
-                                            {nombre && apellidos &&
-                                            <React.Fragment>
-                                                {nombre} {apellidos}
-                                            </React.Fragment>
-                                            }
-                                        </h2>
-                                    </div>
-                            );
-                                                                            }
-                                                                }/>
-
-                        <Route component={error} />
-
-                    </Switch>
-
-                    <Sidebar />
-                </div>
-                    <div className="clearfix"></div>
-
-
-
-                    {/*Footer */}
-                    <Footer></Footer>
-                    {/* Fin Footer */}
-
-                </BrowserRouter>
-        );
-    }
-
+class Router extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        {/* Configurar rutas */}
+        <Switch>
+          <Route exa path="/" component={Peliculas} />
+          <Route path="/prueba-ruta" component={SeccionPruebas} />
+          <Route path="/segunda-ruta" component={MiComponente} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default Router;
+export default React;
